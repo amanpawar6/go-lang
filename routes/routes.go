df60ee54e0a9170bd0446e8a2337b56688bff5d5
Aman Pawar <aman.pawar.7.1997@gmail.com>
2021-06-11 15:52:13 +0530

    api path corrected

--- routes/routes.go
+++ routes/routes.go
@@ -5,7 +5,10 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func handleRequests(app *fiber.App) {
+func handleRequests(a *fiber.App) {
+
+	app := a.Group("/api/v1")
+
 	app.Get("/", controller.HomePage)
 	app.Post("/test", controller.TestApi)
 	app.Get("/users", controller.UsersDetails)
@@ -17,6 +20,7 @@ func handleRequests(app *fiber.App) {
 	app.Put("/updatepassword", controller.UpdatePassword)
 	app.Put("/forgotpassword", controller.ForgetPassword)
 	app.Post("/bulkupload", controller.Bulkupload)
+
 }
 
 func Routes(app *fiber.App) {
